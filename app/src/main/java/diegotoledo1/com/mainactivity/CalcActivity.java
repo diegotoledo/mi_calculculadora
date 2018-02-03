0ff2498abe3749e16a6defd82f526bb585228c9c
diegotoledo <diegotoledo20148@gmail.com>
2018-02-02 18:18:06 -0600

    Proyecto

---
+++ app/src/main/java/diegotoledo1/com/mainactivity/CalcActivity.java
@@ -0,0 +1,30 @@
+package diegotoledo1.com.mainactivity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+import butterknife.BindView;
+
+public class CalcActivity extends AppCompatActivity {
+    @BindView(R.id.valor1)
+    EditText valor1;
+    @BindView(R.id.valor2)
+    EditText valor2;
+    
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
---
+++ app/src/main/java/diegotoledo1/com/mainactivity/MainActivity.java
@@ -0,0 +1,22 @@
+package diegotoledo1.com.mainactivity;
+
+import
+import android.support.annotation.BinderThread;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    @BinderThread (R.id.valor1)
+    EditText valor1;
+    @bindView (R.id.valor2)
+    EditText valor2;
+    @blindView (R.id.resultados)
+
+    @Override protected void onCreate(Bundle savedInstanceState)
+
+    {super.onCreate(savedInstanceState);
+        set
+}ContentView(R.layout.activity_main);
+}
+
+
+
